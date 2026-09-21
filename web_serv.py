579fb231b4b5c909871c00f1b9dc345b1eeb54b4
Tasamu <tasamu@tasamu.com>
2026-09-21 17:06:17 +0900

    文字化け対応

--- web_serv.py
+++ web_serv.py
@@ -125,4 +125,4 @@ def add_image_to_html(response: Response) -> Response:
     return response
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True, use_reloader=False)
